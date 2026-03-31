7aaefc034d768e1f5df6c1f1e82bd2f65a0f6472
Aryan Rai <raia55295@gmail.com>
2026-03-31 23:18:21 +0530

    index

---
+++ js/surprise.js
@@ -0,0 +1,11 @@
+let dotsElem = document.getElementById('dots');
+let count = 1;
+
+setInterval(() => {
+    count = (count % 3) + 1;
+    dotsElem.innerHTML = '.'.repeat(count);
+}, 400);
+
+setTimeout(() => {
+    window.location.href = 'recipe.html';
+}, 2500);
